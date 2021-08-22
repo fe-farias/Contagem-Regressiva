74cdca2db550cabc4af8dd95d865f6d87809047b
Felipe Farias <fefarias97@gmail.com>
2021-08-22 20:24:51 -0300

    Resposta do exercício.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace contagem_regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadLine();
+            Console.WriteLine("Apontar...");
+            Console.ReadLine();
+            Console.WriteLine("Fogo!!!");
+        }
+    }
+}
